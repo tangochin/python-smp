157643d39f1b2f009d3bea4b633309893223f44e
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-02 19:52:30 +0300

    useuse smp_commons.celery.task

--- app_credentials/tasks.py
+++ app_credentials/tasks.py
@@ -1,11 +1,10 @@
-from celery import shared_task
-
 from smp_commons.mq import subscribe
+from smp_commons.celery import task
 
 from . import models
 
 
 @subscribe('platform-auth/app:deleted')
-@shared_task(ignore_result=True)
+@task()
 def handle_platform_app_deleted(platform_app):
     models.Credential.objects.filter(owner_id=platform_app['id']).delete()
