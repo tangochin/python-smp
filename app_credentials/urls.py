f78e1696302705897a152ebc5c1f0ee871abffaa
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-06-14 20:55:42 +0300

    add swagger support

--- app_credentials/urls.py
+++ app_credentials/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+
+from smp_commons.views import get_open_api_view
 from smp_commons.routers import SmpRouter
 
 from . import views
@@ -7,3 +10,5 @@ router = SmpRouter()
 router.register('', views.CredentialViewSet, base_name='credential')
 
 urlpatterns = router.urls
+
+urlpatterns.append(path('swagger.json', get_open_api_view(urlpatterns)))
