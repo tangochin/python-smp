071f21b42558dfa2a6c1c80e6ea591e21e42030d
Nikita Kurinnyi <f1aky927@gmail.com>
2018-06-21 13:12:57 +0300

    added credential filter

--- app_credentials/views.py
+++ app_credentials/views.py
@@ -1,10 +1,28 @@
 from rest_framework import viewsets
+from utils.django import filters
 
 from . import models, serializers
 
 
+class CredentialFilter(filters.FilterSet):
+    class Meta:
+        model = models.Credential
+        fields = {
+            'id': ['exact', 'in'],
+
+            'created_at': ['exact', 'isnull', 'gt', 'gte', 'lt', 'lte'],
+            'updated_at': ['exact', 'isnull', 'gt', 'gte', 'lt', 'lte'],
+
+            'medium': ['exact'],
+            'key': ['exact'],
+            'app_id': ['exact', 'in'],
+        }
+
+
 class CredentialViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CredentialSerializer
+    filter_class = CredentialFilter
+    ordering_fields = ('created_at', )
 
     def get_queryset(self):
         return models.Credential.objects.filter(owner_id=self.request.auth.app_id)
