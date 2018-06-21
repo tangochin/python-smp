0c73618a39a7794976b04909b5fa4c198d184262
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-06-21 22:24:20 +0300

    use common urls from smp_base_django

--- app_credentials/urls.py
+++ app_credentials/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path
-
-from smp_base_django.views import get_open_api_view
+from smp_base_django.urls import urlpatterns
 from smp_base_django.routers import SmpRouter
 
 from . import views
@@ -9,6 +7,4 @@ from . import views
 router = SmpRouter()
 router.register('credentials', views.CredentialViewSet, base_name='credential')
 
-urlpatterns = router.urls
-
-urlpatterns.append(path('swagger.json', get_open_api_view(urlpatterns)))
+urlpatterns += router.urls
