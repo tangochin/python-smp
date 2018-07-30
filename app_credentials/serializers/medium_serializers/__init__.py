ea5cdfdaab422ddaaed3eb794ee13bd06db74cb5
Nikita Kurinnyi <f1aky927@gmail.com>
2018-07-30 13:29:30 +0300

    move registry from python-utils to ./utils

--- app_credentials/serializers/medium_serializers/__init__.py
+++ app_credentials/serializers/medium_serializers/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa: E402, F401
-from utils.registry import Registry
+from ...utils.registry import MediumSerializersRegistry
 
-medium_extra_credential_serializers = Registry(key_name='medium_id')
+medium_extra_credential_serializers = MediumSerializersRegistry(key_name='medium_id')
 
 from .ok import OkMidiumExtraCredentialSerializer
