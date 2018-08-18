de7d55c7f42404f17e436c3ea4c5970ed94a0def
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-08-18 19:27:58 +0300

    don't use WriteableFieldsMixin

--- app_credentials/serializers/CredentialSerializer.py
+++ app_credentials/serializers/CredentialSerializer.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from smp_base_django.serializers import OwnerMixin
 from smp_base_django.utils import wrap_media_errors
 from utils.django.serializers.fields import ChoiceDisplayField
-from utils.django.serializers.mixins import WriteableFieldsMixin
 
 from .. import models
 from ..logic import initialize
@@ -11,11 +10,24 @@ from ..logic import initialize
 from .medium_serializers import medium_extra_credential_serializers
 
 
-class CredentialSerializer(OwnerMixin, WriteableFieldsMixin, serializers.ModelSerializer):
+class CredentialSerializer(OwnerMixin, serializers.ModelSerializer):
     class Meta:
         model = models.Credential
-        fields = ('id', 'owner_id', 'medium', 'app_id', 'key', 'secret', 'extra', 'scope', 'created_at', 'updated_at')
-        writable_fields = ('owner_id', 'medium', 'key', 'secret', 'extra', 'scope')
+        fields = (
+            'id',
+            'owner_id',
+            'medium',
+            'app_id',
+            'key',
+            'secret',
+            'extra',
+            'scope',
+            'created_at',
+            'updated_at',
+        )
+        read_only_fields = (
+            'app_id',
+        )
 
     serializer_choice_field = ChoiceDisplayField
 
