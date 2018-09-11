d85491746549eb85001b2c9919af2c42027cfeac
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-09-11 14:07:18 +0300

    fix initialization logic: don't use smp.get_media_client

--- app_credentials/logic.py
+++ app_credentials/logic.py
@@ -3,12 +3,14 @@ from . import serializers
 
 def initialize(credential, request):
     smp = request.smp
-    serializer = serializers.CredentialSerializer(credential)
-    client = smp.get_media_client(serializer.data)
+    medium = credential.medium.name
 
-    fetched_app = client.post('v1/get-app-using-app-credential')
+    fetched_app = smp.post(f'client-{medium}/v1/get-app-using-app-credential', json={
+        'credential': serializers.CredentialSerializer(credential).data,
+    })
+
     if fetched_app is not None:
-        app = smp.patch(f'apps/v1/by-external-id/{client.medium}:{fetched_app["external_id"]}',
+        app = smp.patch(f'apps/v1/by-external-id/{medium}:{fetched_app["external_id"]}',
                         json=fetched_app)
         credential.app_id = app['id']
     else:
